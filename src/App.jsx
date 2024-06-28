dfeada65c7130fe66217a6e74f3022337b242e3e
lycuong99 <lycuong99@gmail.com>
2024-06-28 16:04:34 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/no-unknown-property */
+import "./App.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { getProject, val } from "@theatre/core";
+import studio from "@theatre/studio";
+import { editable as e, SheetProvider, PerspectiveCamera, useCurrentSheet } from "@theatre/r3f";
+import { Gltf, ScrollControls, useScroll } from "@react-three/drei";
 
-function App() {
-  const [count, setCount] = useState(0)
+studio.initialize();
 
+// our Theatre.js project sheet, we'll use this later
+const demoSheet = getProject("Fly Through").sheet("Scene");
+
+const App = () => {
+  return (
+    <Canvas
+    gl={{ preserveDrawingBuffer: true }}
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <ScrollControls pages={5}>
+        <SheetProvider sheet={demoSheet}>
+          <Scene />
+        </SheetProvider>
+      </ScrollControls>
+    </Canvas>
+  );
+};
+
+const Scene = () => {
+  const sheet = useCurrentSheet();
+  const scroll = useScroll();
+
+  // our callback will run on every animation frame
+  useFrame(() => {
+    // the length of our sequence
+    const sequenceLength = val(sheet.sequence.pointer.length);
+    // update the "position" of the playhead in the sequence, as a fraction of its whole length
+    sheet.sequence.position = scroll.offset * sequenceLength;
+  });
+
+  const bgColor = "#84a4f4";
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <color attach="background" args={[bgColor]} />
+      <fog attach="fog" color={bgColor} near={-4} far={10} />
 
-export default App
+     
+
+      <ambientLight intensity={0.5} />
+      <e.pointLight theatreKey="Light" position={[10, 10, 10]} />
+      <directionalLight position={[-5, 5, -5]} intensity={1.5} />
+
+
+      <Gltf src="/environment.glb" castShadow receiveShadow />
+
+
+      <PerspectiveCamera theatreKey="Camera" makeDefault position={[0, 0, 0]} fov={85} near={0.1} far={70} />
+     
+    </>
+  );
+};
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,16 @@
-import * as THREE from "three";
 import { createRoot } from "react-dom/client";
-import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { getProject } from "@theatre/core";
 import studio from "@theatre/studio";
 import extension from "@theatre/r3f/dist/extension";
-import { editable as e, SheetProvider } from "@theatre/r3f";
-import { PerspectiveCamera } from "@theatre/r3f";
+import App from "./App";
+import React, { Suspense } from "react";
 
 studio.initialize();
 studio.extend(extension);
 
-// our Theatre.js project sheet, we'll use this later
-const demoSheet = getProject("Demo Project").sheet("Demo Sheet");
-
-const App = () => {
-  return (
-    <Canvas
-      style={{
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <SheetProvider sheet={demoSheet}>
-        <PerspectiveCamera theatreKey="Camera" makeDefault position={[5, 5, -5]} fov={75} />
-
-        <ambientLight />
-        <e.pointLight theatreKey="Light" position={[10, 10, 10]} />
-        <e.mesh theatreKey="Cube">
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="orange" />
-        </e.mesh>
-      </SheetProvider>
-    </Canvas>
-  );
-};
-
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+  </React.StrictMode>
+);
